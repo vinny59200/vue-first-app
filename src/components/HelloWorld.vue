6b4829cb3fbcc2f72ca221b3dedd4ed0c1e54c25
vvauban <vvauban@gmail.com>
2024-02-20 02:36:57 +0100

    Add color change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,14 +4,33 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const cardBackgroundColor = ref('');
+
+const getRandomColor = () => {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    };
+
+
+const doVV = () => {
+  count.value++;
+  cardBackgroundColor.value =getRandomColor();
+}
+
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-
+  <div  v-bind:style="{border: 1,backgroundColor: cardBackgroundColor}">
+    <br><br><br><br><br>
+    <button type="button" @click="doVV()">count is {{ count }}</button>
+    <br><br><br><br><br>
   </div>
 </template>
 
